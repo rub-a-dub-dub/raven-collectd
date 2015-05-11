863b7fae9538be002f56ef00c4980d17bda52b32
rub-a-dub-dub <rub-a-dub-dub@users.noreply.github.com>
2015-05-10 21:32:12 -0700

    Refactored imported code and got collectd hooks into it.

--- ravencollectd.py
+++ ravencollectd.py
@@ -2,122 +2,113 @@ import sys
 import time
 import re
 import xml.etree.ElementTree as ET
-import logging as log
 import serial
+import collectd
 
-class ravencollectd:
-    '''This class handles all communication to/from the RAVEn and the MQTT     broker'''
+serDevice = "/dev/ttyUSB0"
+ser = None
+reStartTag = re.compile('^<[a-zA-Z0-9]+>')
+reEndTag = re.compile('^<\/[a-zA-Z0-9]+>')
 
-    def __init__(self, serDevice, hostName, hostPort, hostUser, hostPwd, topic):
-        '''The constructor requires all connection information for both MQTT and the RAVEn'''
-        self.serDevice = serDevice
-        self.ser = None
+def close_plugin:
+    '''This will clean up all opened connections'''
+    if ser is not None:
+        ser.close()
+        collectd.info("Serial port closed.")
+    else:
+        collectd.debug("Asking to close serial port, but it was never open.")
 
-        # Various Regex's
-        self.reStartTag = re.compile('^<[a-zA-Z0-9]+>') # to find a start XML tag (at very beginning of line)
-        self.reEndTag = re.compile('^<\/[a-zA-Z0-9]+>') # to find an end XML tag (at very beginning of line)
+def initialise_plugin:
+    '''This function opens the serial port looking for a RAVEn. Returns True if successful, False otherwise.'''
+    try:
+        ser = serial.Serial(serDevice, 115200, serial.EIGHTBITS, serial.PARITY_NONE, timeout=0.5)
+        ser.close()
+        ser.open()
+        ser.flushInput()
+        ser.flushOutput()
+        collectd.info("Connected to: " + ser.portstr)
+        return True
+    except Exception as e:
+        collectd.error("Cannot open serial port: " + str(e))
+        return False
 
-    def __del__(self):
-        '''This will close all connections (serial/MQTT)'''
-        self.close()
+def isReady:
+    '''This function is used to check if this object has been initialised correctly and is ready to process data'''
+    return (ser is not None)
 
-    def _openSerial(self):
-        '''This function opens the serial port looking for a RAVEn. Returns True if successful, False otherwise.'''
-        try:
-            self.ser = serial.Serial(self.serDevice, 115200, serial.EIGHTBITS, serial.PARITY_NONE, timeout=0.5)
-            self.ser.close()
-            self.ser.open()
-            self.ser.flushInput()
-            self.ser.flushOutput()
-            log.info("Connected to: " + self.ser.portstr)
-            return True
-        except Exception as e:
-            log.critical("Cannot open serial port: " + str(e))
-            return False
+def getInstantDemandKWh(xmltree):
+    '''Returns a single float value for the Demand from an Instantaneous Demand response from RAVEn'''
+    # Get the Instantaneous Demand
+    fDemand = float(int(xmltree.find('Demand').text,16))
+    fResult = calculateRAVEnNumber(xmltree, fDemand)
+    return fResult
 
-    def _closeSerial(self):
-        '''This function will close the serial port talking to the RAVEn'''
-        if self.ser is not None:
-            self.ser.close()
-            log.info("Serial port closed.")
-        else:
-            log.debug("Asking to close serial port, but it was never open.")
+def calculateRAVEnNumber(xmltree, value):
+    '''Calculates a float value from RAVEn using Multiplier and Divisor in XML response'''
+    # Get calculation parameters from XML - Multiplier, Divisor
+    fDivisor = float(int(xmltree.find('Divisor').text,16))
+    fMultiplier = float(int(xmltree.find('Multiplier').text,16))
+    if (fMultiplier > 0 and fDivisor > 0):
+        fResult = float( (value * fMultiplier) / fDivisor)
+    elif (fMultiplier > 0):
+        fResult = float(value * fMultiplier)
+    else: # (Divisor > 0) or anything else
+        fResult = float(value / fDivisor)
+    return fResult*1000
 
-    def open(self):
-        '''This function will open all necessary connections for the RAVEn to talk to the MQTT broker'''
-        if not self._openSerial():
-            log.critical("Serial port was not opened due to an error.")
-            return False
-        else:
-		  return True
+def write_to_collectd(dataPt):
+    '''This actually writes the data to collectd'''
+    val = collectd.Values(plugin='ravencollectd')
+    val.type  = 'gauge'
+    val.type_instance = 'instantdemand'
+    val.plugin_instance = 'raven'
+    val.values = [dataPt]
+    val.dispatch()
 
-    def close(self):
-        '''This function will close all previously opened connections'''
-        if self.ser is not None: self._closeSerial()
+def read_data:
+    '''This function will read from the serial device, process the data and publish MQTT messages'''
+    if isReady():
+        # begin listening to RAVEn
+        rawxml = ""
 
-    def _isReady(self):
-        '''This function is used to check if this object has been initialised correctly and is ready to process data'''
-        return  (self.ser is not None)
-
-    def run(self):
-        '''This function will read from the serial device, process the data and publish MQTT messages'''
-        if self._isReady():
-            # begin listening to RAVEn
-            rawxml = ""
-
-            while True:
-                # wait for /n terminated line on serial port (up to timeout)
-                rawline = self.ser.readline()
-                # remove null bytes that creep in immediately after connecting
-                rawline = rawline.strip('\0')
-                # only bother if this isn't a blank line
-                if len(rawline) > 0:
-                    # start tag
-                    if self.reStartTag.match(rawline):
-                        rawxml = rawline
-                        log.debug("Start XML Tag found: " + rawline)
-                    # end tag
-                    elif self.reEndTag.match(rawline):
-                        rawxml = rawxml + rawline
-                        log.debug("End XML Tag Fragment found: " + rawline)
-                        try:
-                            xmltree = ET.fromstring(rawxml)
-                            if xmltree.tag == 'InstantaneousDemand':
-                                self.client.publish(self.topic, payload=self._getInstantDemandKWh(xmltree), qos=0)
-                                log.debug(self._getInstantDemandKWh(xmltree))
-                            else:
-                                log.warning("*** Unrecognised (not implemented) XML Fragment")
-                                log.warning(rawxml)
-                        except Exception as e:
-                          log.error("Exception triggered: " + str(e))
-                        # reset rawxml
-                        rawxml = ""
-                    # if it starts with a space, it's inside the fragment
-                    else:
-                        rawxml = rawxml + rawline
-                        log.debug("Normal inner XML Fragment: " + rawline)
+        while True:
+            # wait for /n terminated line on serial port (up to timeout)
+            rawline = ser.readline()
+            # remove null bytes that creep in immediately after connecting
+            rawline = rawline.strip('\0')
+            # only bother if this isn't a blank line
+            if len(rawline) > 0:
+                # start tag
+                if reStartTag.match(rawline):
+                    rawxml = rawline
+                    collectd.debug("Start XML Tag found: " + rawline)
+                # end tag
+                elif reEndTag.match(rawline):
+                    rawxml = rawxml + rawline
+                    collectd.debug("End XML Tag Fragment found: " + rawline)
+                    try:
+                        xmltree = ET.fromstring(rawxml)
+                        if xmltree.tag == 'InstantaneousDemand':
+                            write_to_collectd(getInstantDemandKWh(xmltree))
+                            # collectd.debug(getInstantDemandKWh(xmltree))
+                        else:
+                            collectd.warning("Unrecognised (not implemented) XML Fragment")
+                            collectd.warning(rawxml)
+                    except Exception as e:
+                      collectd.error("Exception triggered: " + str(e))
+                    # reset rawxml
+                    rawxml = ""
+                    return
+                # if it starts with a space, it's inside the fragment
                 else:
-		  pass
+                    rawxml = rawxml + rawline
+                    collectd.debug("Normal inner XML Fragment: " + rawline)
+            else:
+                pass
+    else:
+        collectd.error("Was asked to begin reading/writing data without opening connections.")
 
-        else:
-            log.error("Was asked to begin reading/writing data without opening connections.")
 
-    def _getInstantDemandKWh(self, xmltree):
-        '''Returns a single float value for the Demand from an Instantaneous Demand response from RAVEn'''
-        # Get the Instantaneous Demand
-        fDemand = float(int(xmltree.find('Demand').text,16))
-        fResult = self._calculateRAVEnNumber(xmltree, fDemand)
-        return fResult
-
-    def _calculateRAVEnNumber(self, xmltree, value):
-        '''Calculates a float value from RAVEn using Multiplier and Divisor in XML response'''
-        # Get calculation parameters from XML - Multiplier, Divisor
-        fDivisor = float(int(xmltree.find('Divisor').text,16))
-        fMultiplier = float(int(xmltree.find('Multiplier').text,16))
-        if (fMultiplier > 0 and fDivisor > 0):
-            fResult = float( (value * fMultiplier) / fDivisor)
-        elif (fMultiplier > 0):
-            fResult = float(value * fMultiplier)
-        else: # (Divisor > 0) or anything else
-            fResult = float(value / fDivisor)
-        return fResult*1000
+collectd.register_init(initialise_plugin)
+collectd.register_read(read_data)
+collectd.register_shutdown(close_plugin)
