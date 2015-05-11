6ab734a2284a407e7e65d7025fa382e3cef5fb93
rub-a-dub-dub <rub-a-dub-dub@users.noreply.github.com>
2015-05-10 22:27:51 -0700

    Added comment header for file.

--- ravencollectd.py
+++ ravencollectd.py
@@ -1,3 +1,34 @@
+# ravencollectd - ravencollectd.py
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; only version 2 of the License is applicable.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# 
+# Authors:
+#   rub-a-dub-dub @ github
+#
+# About this plugin:
+#   This is a plugin for collectd using its Python interface to read data from
+#   a Rainforest Automation USB dongle (the RAVEn RFA-Z106).
+# 
+# collectd:
+#   http://collectd.org
+# collectd-python:
+#   http://collectd.org/documentation/manpages/collectd-python.5.shtml
+# Rainforest Automation RAVEn RFA-Z106:
+#   http://rainforestautomation.com/rfa-z106-raven/
+#
+
 import sys
 import time
 import re
